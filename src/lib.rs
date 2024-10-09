65c53c16b4a778960084eb844e4e592ff1ad3e60
Naxdy <naxdy@polskalemon.com>
2024-10-09 12:09:39 +0200

    chore: fix clippy issues

--- src/lib.rs
+++ src/lib.rs
@@ -84,33 +84,31 @@ unsafe fn non_hdr_update_room_hook(_: &skyline::hooks::InlineCtx) {
             CURRENT_PANE_HANDLE as u64,
             format!("ID: {}\0", CURRENT_ARENA_ID).as_ptr(),
         );
-    } else {
-        if CURRENT_INPUT_BUFFER == -1 {
-            if MOST_RECENT_AUTO == -1 {
-                set_text_string(
-                    CURRENT_PANE_HANDLE as u64,
-                    format!("ID: {}\nInput Latency: Auto\0", CURRENT_ARENA_ID).as_ptr(),
-                );
-            } else {
-                set_text_string(
-                    CURRENT_PANE_HANDLE as u64,
-                    format!(
-                        "ID: {}\nInput Latency: Auto ({})\0",
-                        CURRENT_ARENA_ID, MOST_RECENT_AUTO
-                    )
-                    .as_ptr(),
-                )
-            }
+    } else if CURRENT_INPUT_BUFFER == -1 {
+        if MOST_RECENT_AUTO == -1 {
+            set_text_string(
+                CURRENT_PANE_HANDLE as u64,
+                format!("ID: {}\nInput Latency: Auto\0", CURRENT_ARENA_ID).as_ptr(),
+            );
         } else {
             set_text_string(
                 CURRENT_PANE_HANDLE as u64,
                 format!(
-                    "ID: {}\nInput Latency: {}\0",
-                    CURRENT_ARENA_ID, CURRENT_INPUT_BUFFER
+                    "ID: {}\nInput Latency: Auto ({})\0",
+                    CURRENT_ARENA_ID, MOST_RECENT_AUTO
                 )
                 .as_ptr(),
-            );
+            )
         }
+    } else {
+        set_text_string(
+            CURRENT_PANE_HANDLE as u64,
+            format!(
+                "ID: {}\nInput Latency: {}\0",
+                CURRENT_ARENA_ID, CURRENT_INPUT_BUFFER
+            )
+            .as_ptr(),
+        );
     }
 }
 
@@ -119,7 +117,7 @@ unsafe fn handle_draw_hook(layout: *mut Layout, draw_info: u64, cmd_buffer: u64)
     if IS_CSS {
         let root_pane = &mut *(*layout).root_pane;
 
-        draw_ui(&root_pane);
+        draw_ui(root_pane);
     }
 
     call_original!(layout, draw_info, cmd_buffer);
@@ -184,7 +182,7 @@ unsafe fn draw_ui(root_pane: &Pane) {
     let vip_pane_00 = root_pane.find_pane_by_name_recursive("txt_vip_title_00");
     let vip_pane_01 = root_pane.find_pane_by_name_recursive("txt_vip_title_01");
 
-    if ORIG_VIP_TEXT.is_empty() || ORIG_VIP_TEXT.len() <= 0 {
+    if ORIG_VIP_TEXT.is_empty() {
         match (vip_pane_00, vip_pane_01) {
             (Some(x), _) | (_, Some(x)) => {
                 ORIG_VIP_TEXT = dbg!(String::from_utf16(std::slice::from_raw_parts(
@@ -195,29 +193,22 @@ unsafe fn draw_ui(root_pane: &Pane) {
             }
             _ => (),
         }
-    } else {
-        match (vip_pane_00, vip_pane_01) {
-            (Some(x), Some(y)) => {
-                let s = if !STEALTH_MODE {
-                    if CURRENT_INPUT_BUFFER != -1 {
-                        format!("Input Latency: {}", CURRENT_INPUT_BUFFER)
-                    } else {
-                        if MOST_RECENT_AUTO == -1 {
-                            format!("Input Latency: Auto")
-                        } else {
-                            format!("Input Latency: Auto ({})", MOST_RECENT_AUTO)
-                        }
-                    }
-                } else {
-                    ORIG_VIP_TEXT.clone()
-                };
-
-                for e in [x, y] {
-                    e.as_textbox().set_text_string(s.as_str());
-                }
+    } else if let (Some(x), Some(y)) = (vip_pane_00, vip_pane_01) {
+        let s = if !STEALTH_MODE {
+            if CURRENT_INPUT_BUFFER != -1 {
+                format!("Input Latency: {}", CURRENT_INPUT_BUFFER)
+            } else if MOST_RECENT_AUTO == -1 {
+                "Input Latency: Auto".to_string()
+            } else {
+                format!("Input Latency: Auto ({})", MOST_RECENT_AUTO)
             }
-            _ => (),
-        }
+        } else {
+            ORIG_VIP_TEXT.clone()
+        };
+
+        [x, y]
+            .into_iter()
+            .for_each(|e| e.as_textbox().set_text_string(s.as_str()));
     }
 }
 
